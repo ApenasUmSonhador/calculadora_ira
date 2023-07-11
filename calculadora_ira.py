e4d0092d3062bb9d7193fa51b3abcd4efeaab2fe
Arthur Vinicius Carneiro Nunes <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-07-11 20:25:09 -0300

    Add files via upload

---
+++ calculadora_ira.py
@@ -0,0 +1,110 @@
+lista_de_cadeiras = []
+
+
+# Funções.
+# Função que será responsável pela criação dos dicionários das disciplinas
+def nova_disciplina():
+    codigo = input("Digite o Código da disciplina: ")
+    status = (
+        (
+            input(
+                "Qual o Status da disciplina? \n\
+                    [A]Aprovada [B]Reprovada por nota [C]Reprovada por falta [D]Trancada"
+            )
+        )
+        .strip()
+        .title()
+    )
+    carga = int(input("Digite a carga horária da disciplina ao todo no semestre: "))
+    periodo = int(
+        input(
+            "Digite o semestre (adotando o semestre 1 o que foi de seu ingresso na UFC) em que fez essa disciplina"
+        )
+    )
+    if status.startswith("A"):
+        status = "Aprovado"
+    elif status.startswith("B"):
+        status = "Reprovado por nota"
+    elif status.startswith("C"):
+        status = "Reprovado por falta"
+    elif status.startswith("D"):
+        status = "Trancado"
+    if status != "Trancado":
+        nota = float(input("Digite a nota final na disciplina."))
+
+    disciplina = {
+        "codigo": codigo,
+        "status": status,
+        "carga horaria": carga,
+        "periodo": periodo,
+        "nota final": "Trancado" if status == "Trancado" else nota,
+    }
+    lista_de_cadeiras.append(disciplina)
+
+
+# Função que fará o calculo do IRA
+def calcula_IRA():
+    def somatorio_de_chave(chave):
+        somatorio = 0
+        if chave == "trancadas":
+            for cadeira in lista_de_cadeiras:
+                if cadeira["status"] == "Trancado":
+                    somatorio += cadeira["carga horaria"]
+
+        elif chave == "carga":
+            for cadeira in lista_de_cadeiras:
+                somatorio += cadeira["carga horaria"]
+
+        elif chave == "nota final * periodo * carga horaria":
+            for cadeira in lista_de_cadeiras:
+                if (
+                    cadeira["status"] != "Reprovado por falta"
+                    and cadeira["status"] != "Trancado"
+                ):
+                    somatorio += (
+                        cadeira["periodo"]
+                        * cadeira["carga horaria"]
+                        * cadeira["nota final"]
+                    )
+
+        elif chave == "carga horaria * periodo":
+            for cadeira in lista_de_cadeiras:
+                if cadeira["status"] != "Trancado":
+                    somatorio += cadeira["periodo"] * cadeira["carga horaria"]
+
+        elif chave == "carga":
+            for cadeira in lista_de_cadeiras:
+                somatorio += cadeira["carga horaria"]
+
+        return somatorio
+
+    t = somatorio_de_chave("trancadas")
+    c = somatorio_de_chave("carga")
+    s1 = somatorio_de_chave("nota final * periodo * carga horaria")
+    s2 = somatorio_de_chave("carga horaria * periodo")
+
+    ira = (1 - 0.5 * (t / c)) * (s1 / s2) * 1000
+
+    return ira
+
+
+# Código principal
+while True:
+    nova_disciplina()
+    decisao = (
+        (input("Existe alguma outra disciplina a ser adicionada?\n [S]Sim [N]Nao: "))
+        .strip()
+        .title()
+        .startswith("N")
+    )
+    if decisao == True:
+        break
+
+ira = calcula_IRA()
+print(f"Seu IRA é de {ira}.")
+print()
+print("DISCIPLINAS:")
+print()
+for disciplina in lista_de_cadeiras:
+    print(disciplina)
+    print()
